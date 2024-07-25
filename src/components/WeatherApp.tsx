9b9e6944139b185e0390a5640a651a688eccf7af
Julia <veluat@gmail.com>
2024-07-25 21:49:19 +0300

    fix: Prevent infinite re-rendering and unnecessary API requests

--- src/components/WeatherApp.tsx
+++ src/components/WeatherApp.tsx
@@ -1,29 +1,39 @@
-import React, {useEffect, useState} from 'react';
-import s from './WeatherApp.module.scss';
-import search_icon from '../assets/search.png';
-import cloud_icon from '../assets/cloud.png';
-import wind_icon from '../assets/wind.png';
-import humidity_icon from '../assets/humidity.png';
-import clear_icon from '../assets/clear.png';
-import drizzle_icon from '../assets/drizzle.png';
-import snow_icon from '../assets/snow.png';
-import rain_icon from '../assets/rain.png';
-import useWeatherData from "../hook/useWeatherData";
+import React, {useEffect, useState} from 'react'
+import s from './WeatherApp.module.scss'
+import search_icon from '../assets/search.png'
+import cloud_icon from '../assets/cloud.png'
+import wind_icon from '../assets/wind.png'
+import humidity_icon from '../assets/humidity.png'
+import clear_icon from '../assets/clear.png'
+import drizzle_icon from '../assets/drizzle.png'
+import snow_icon from '../assets/snow.png'
+import rain_icon from '../assets/rain.png'
+import useWeatherData from '../hook/useWeatherData'
 
 export const WeatherApp: React.FC = () => {
-  const [location, setLocation] = useState('Minsk');
-  const { weatherData, error, fetchWeatherData } = useWeatherData();
+  const [location, setLocation] = useState('Minsk')
+  const {weatherData, error, fetchWeatherData, isLoading} = useWeatherData()
 
   useEffect(() => {
-    const fetchData = async () => {
-      await fetchWeatherData(location);
-    };
-    void fetchData();
-  }, [fetchWeatherData, location]);
+    if (!weatherData && !isLoading) {
+      const fetchData = async () => {
+        await fetchWeatherData(location)
+      }
+      void fetchData()
+    }
+  }, [fetchWeatherData, location, weatherData, isLoading])
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setLocation(event.currentTarget.value)
+  }
 
   const handleSearch = async () => {
-    await fetchWeatherData(location);
-  };
+    try {
+      await fetchWeatherData(location)
+    } catch (err) {
+      console.error('Error fetching weather data:', err)
+    }
+  }
 
   if (!weatherData) {
     return (
@@ -34,73 +44,73 @@ export const WeatherApp: React.FC = () => {
           <div className={s.loadingMessage}>Loading...</div>
         )}
       </div>
-    );
+    )
   }
 
-  const {main, wind, name, weather} = weatherData;
-  const {temp, humidity} = main;
-  const {speed} = wind;
-  const [{icon}] = weather;
+  const {main, wind, name, weather} = weatherData
+  const {temp, humidity} = main
+  const {speed} = wind
+  const [{icon}] = weather
 
-  let weatherIconSrc;
+  let weatherIconSrc
   switch (icon) {
     case '01d':
     case '01n':
-      weatherIconSrc = clear_icon;
-      break;
+      weatherIconSrc = clear_icon
+      break
     case '02d':
     case '02n':
-      weatherIconSrc = cloud_icon;
-      break;
+      weatherIconSrc = cloud_icon
+      break
     case '03d':
     case '03n':
     case '04d':
     case '04n':
-      weatherIconSrc = drizzle_icon;
-      break;
+      weatherIconSrc = drizzle_icon
+      break
     case '09d':
     case '09n':
     case '10d':
     case '10n':
-      weatherIconSrc = rain_icon;
-      break;
+      weatherIconSrc = rain_icon
+      break
     case '13d':
     case '13n':
-      weatherIconSrc = snow_icon;
-      break;
+      weatherIconSrc = snow_icon
+      break
     default:
-      weatherIconSrc = clear_icon;
+      weatherIconSrc = clear_icon
   }
 
   return (
     <div className={s.container}>
       <div className={s.topBar}>
         <input
-          type="text"
+          type='text'
           className={s.cityInput}
-          placeholder="Search"
+          placeholder='Search'
           value={location}
-          onChange={(event) => setLocation(event.currentTarget.value)}
+          onChange={handleInputChange}
         />
         <div className={s.seachIcon} onClick={handleSearch}>
-          <img src={search_icon} alt="search"/>
+          <img src={search_icon} alt='search'/>
         </div>
       </div>
       <div className={s.weatherImage}>
-        <img src={weatherIconSrc} alt="weather"/>
+        <img src={weatherIconSrc} alt='weather'/>
       </div>
       <div className={s.weatherTemp}>{Math.round(temp)} °C</div>
       <div className={s.weatherLocation}>{name}</div>
       <div className={s.dataContainer}>
         <div className={s.element}>
-          <img src={humidity_icon} alt="humidity" className={s.icon}/>
+          <img src={humidity_icon} alt='humidity' className={s.icon}/>
           <div className={s.data}>
             <div className={s.humidityPercent}>{humidity} %</div>
             <div className={s.text}>Humidity</div>
           </div>
         </div>
         <div className={s.element}>
-          <img src={wind_icon} alt="wind" className={s.icon}/>
+          <img src={wind_icon} alt='wind' className={s.icon}/>
           <div className={s.data}>
             <div className={s.windRate}>{Math.round(speed)} km/h</div>
             <div className={s.text}>Wind Speed</div>
@@ -108,5 +118,5 @@ export const WeatherApp: React.FC = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
--- src/hook/useWeatherData.ts
+++ src/hook/useWeatherData.ts
@@ -17,6 +17,7 @@ const useWeatherData = () => {
   const [error, setError] = useState<string | null>(null);
   const apiKey = process.env.REACT_APP_API_KEY;
   const [weatherData, setWeatherData] = useState<ResponseWeatherDataType | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchWeatherData = async (location: string) => {
     try {
@@ -24,29 +25,33 @@ const useWeatherData = () => {
         setError('Please enter a location.');
         return;
       }
-
+      setIsLoading(true);
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=Metric&appid=${apiKey}`;
       const response = await fetch(url);
 
       if (!response.ok) {
         const errorData = await response.json();
         setError(`Error: ${errorData.message}`);
+        setIsLoading(false);
         return;
       }
 
       const data = await response.json();
       setWeatherData(data);
       setError(null);
+      setIsLoading(false);
     } catch (error) {
       if (!apiKey) {
         setError('API key is missing. Please set the REACT_APP_API_KEY environment variable.');
+        setIsLoading(false);
         return;
       }
       setError('An error occurred while fetching weather data.');
+      setIsLoading(false);
     }
   };
 
-  return {weatherData, error, fetchWeatherData}
+  return {weatherData, error, fetchWeatherData, isLoading}
 }
 
 export default useWeatherData;
