704598d9bc42f3d546af90dfb4b67e07368c0611
Julia <veluat@gmail.com>
2024-07-27 10:27:13 +0300

    feat: add HourlyWeather

---
+++ src/components/hourly-weather/HourlyWeather.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import s from './HourlyWeather.module.scss'
+
+export const HourlyWeather = () => {
+  return (
+    <div className={s.root}>
+      <h3>Hourly Weather Details
+        <br/>
+        Coming soon</h3>
+    </div>
+  )
+}
