6e5699f52b929123c0cac5b68f2f4afaac68bb7d
Julia <veluat@gmail.com>
2024-07-30 22:13:39 +0300

    feat: updated footer, header

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,14 +1,17 @@
-import React, {useContext, useState} from 'react'
+import React, {useContext} from 'react'
 import s from './Header.module.scss'
 import {LoadingIndicator} from '../../shared/loading-indicator'
 import {ErrorMessage} from '../../shared/error-message'
 import {Settings} from '../../shared/settings'
+import {LocaleContext} from '../../../utils'
+import data from './../../../data/ui-common-data/UiCommonData'
 
 type Props = {
   isLoading: boolean
   error: string | null
 }
 export const Header: React.FC<Props> = ({isLoading, error}) => {
+  const {locale} = useContext(LocaleContext)
 
   return (
     <div className={s.root}>
@@ -16,7 +19,7 @@ export const Header: React.FC<Props> = ({isLoading, error}) => {
       {isLoading && <LoadingIndicator/>}
       {!isLoading && error && <ErrorMessage message={error}/>}
       <div className={s.timeBox}>
-        World Time<br/>Coming Soon
+        {data[locale].time}<br/>{data[locale].dev}
       </div>
     </div>
   )
