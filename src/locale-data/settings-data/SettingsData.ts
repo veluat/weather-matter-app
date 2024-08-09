36837f7b5e8d9e2930c8d90c883b8693d481594f
Julia <veluat@gmail.com>
2024-08-10 01:03:17 +0300

    feat: add settings locale data

---
+++ src/locale-data/settings-data/SettingsData.ts
@@ -0,0 +1,12 @@
+import {SettingsDataType} from './SettingsDataType'
+
+const SettingsData: SettingsDataType = {
+  en: {
+    label: ['time zone', 'language', 'temperature', 'theme']
+  },
+  ru: {
+    label: ['часовой пояс', 'язык', 'температура', 'тема']
+  }
+}
+
+export default SettingsData
